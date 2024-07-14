45fdde533b9b4ddb5cb0edee78170912ee5d62cf
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-14 07:56:25 +0500

    none

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["*.{html, js}"],
   theme: {
+    screens: {
+      xs: "576px", // Extra small (≤576px) [Note: Tailwind defaultda bu breakpointsni taqdim etmaydi, siz qo'shishingiz kerak]
+      sm: "576px", // Small (≥576px)
+      md: "768px", // Medium (≥768px)
+      lg: "992px", // Large (≥992px)
+      xl: "1200px", // Extra large (≥1200px)
+      xxl: "1300px", // Extra extra large (≥1400px)
+    },
     extend: {},
   },
   plugins: [],
